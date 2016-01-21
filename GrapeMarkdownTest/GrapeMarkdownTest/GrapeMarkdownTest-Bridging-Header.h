ba77c1f350a274ee2170c3d6301ea1fff47848d6
Lukas Gauster <lg@ubergrape.com>
2016-01-21 13:58:56 +0100

    initial Commit

---
+++ GrapeMarkdownTest/GrapeMarkdownTest/GrapeMarkdownTest-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  GrapeMarkdownTest-Bridging-Header.h
+//  GrapeMarkdownTest
+//
+//  Created by Lukas Gauster on 21/01/16.
+//  Copyright © 2016 Grape. All rights reserved.
+//
+
+#ifndef GrapeMarkdownTest_Bridging_Header_h
+#define GrapeMarkdownTest_Bridging_Header_h
+
+#import "XNGMarkdownParser.h"
+
+#endif /* GrapeMarkdownTest_Bridging_Header_h */
---
+++ GrapeMarkdownTest/GrapeMarkdownTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrapeMarkdownTest
+//
+//  Created by Lukas Gauster on 21/01/16.
+//  Copyright © 2016 Grape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
